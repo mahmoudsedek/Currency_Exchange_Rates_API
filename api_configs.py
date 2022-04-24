00be0e633855c41d1c27465531dd036d7ddab349
Mahmoud Sedek <75182825+mahmoudsedek@users.noreply.github.com>
2022-04-24 16:31:41 +0200

    Update api_configs.py

--- api_configs.py
+++ api_configs.py
@@ -1,5 +1,5 @@
 URL = "https://openexchangerates.org/api/latest.json?app_id="
-TOKEN = "a8bbcddd31004b9dbdc7786662dae3ad"
+TOKEN = "###########################"
 BASE = "USD"
 SYMBOLS = "USD,EUR,EGP"
 PRECISION = 20
