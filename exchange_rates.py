5a3d26f09f03266a50980b4d0e1d82728016e0da
Mahmoud Sedek <75182825+mahmoudsedek@users.noreply.github.com>
2022-04-24 21:50:14 +0200

    finalizing comments

--- exchange_rates.py
+++ exchange_rates.py
@@ -10,7 +10,7 @@ import json
 
 
 def get_argument() -> argparse.Namespace:
-    """function takes args from the user with the targeted directory for the partioned data"""
+    """function takes args from the user with the targeted directory for the partitioned data and has a default value for the cwd"""
     parser = argparse.ArgumentParser("exchange rate script")
     parser.add_argument("-d", "--dir", default=".", help="directory of the file for the exchange rates, default is current directory")
     args = parser.parse_args()
@@ -18,7 +18,7 @@ def get_argument() -> argparse.Namespace:
 
 
 def get_exchange_rates() -> dict:
-    """function to call the API and get latest exchange rates for specific symbols"""
+    """function to call the API and get the latest exchange rates for specific symbols(that are defined in the api_configs.py file)"""
     full_url = api_configs.URL + api_configs.TOKEN + "&base=" + api_configs.BASE + "&symbols=" + api_configs.SYMBOLS
     try:
         response = requests.get(full_url)
@@ -32,7 +32,7 @@ def get_exchange_rates() -> dict:
 
 
 def convert_rates(data: dict) -> tuple:
-    """function to work-around the currency convertion as the free API subsription doesn't allow to change the base"""
+    """function to work-around the currency conversion as the free API subscription doesn't allow to change the base"""
     data = get_exchange_rates()
     base = "1"
     usd = str(1 / Decimal(data["rates"]["EGP"])) #EGP to USD
@@ -46,7 +46,7 @@ def convert_rates(data: dict) -> tuple:
 
 
 def write_partitioned_data(partition_folder_path: str, partition_file_path: str, header: list, row: list) -> None:
-    """function to check/write data in directories/files"""
+    """function to check first if the folder/file does not exist and create them, then write the header for the 1st time and append any new row of data"""
     file_exist = os.path.isfile(partition_file_path)
     os.makedirs(partition_folder_path, exist_ok=True)
     if not file_exist:
