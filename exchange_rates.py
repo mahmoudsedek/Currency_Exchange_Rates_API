4d968961410bc6fd047293e8e7636ef3e92e51fc
Mahmoud Sedek <mahmoud_sedek@outlook.com>
2022-07-04 18:23:05 +0200

    formatting the code and editing the readme file

--- exchange_rates.py
+++ exchange_rates.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import api_configs
 import requests
 import argparse
@@ -8,41 +9,6 @@ import sys
 import csv
 import json
 
-
-def get_argument() -> argparse.Namespace:
-    """function takes args from the user with the targeted directory for the partitioned data and has a default value for the cwd"""
-    parser = argparse.ArgumentParser("exchange rate script")
-    parser.add_argument("-d", "--dir", default=".", help="directory of the file for the exchange rates, default is current directory")
-    args = parser.parse_args()
-    return args
-
-
-def get_exchange_rates() -> dict:
-    """function to call the API and get the latest exchange rates for specific symbols(that are defined in the api_configs.py file)"""
-    full_url = api_configs.URL + api_configs.TOKEN + "&base=" + api_configs.BASE + "&symbols=" + api_configs.SYMBOLS
-    try:
-        response = requests.get(full_url)
-    except requests.exceptions.RequestException as e:
-        print("Something went wrong with the api call, please check it")
-        print(e)
-        sys.exit(1)
-    json_payload = response.text
-    dict_payload = json.loads(json_payload)
-    return dict_payload
-
-
-def convert_rates(data: dict) -> tuple:
-    """function to work-around the currency conversion as the free API subscription doesn't allow to change the base"""
-    data = get_exchange_rates()
-    base = "1"
-    usd = str(1 / Decimal(data["rates"]["EGP"])) #EGP to USD
-    eur = str((1 / Decimal(data["rates"]["EGP"])) * Decimal(data["rates"]["EUR"])) #EGP to EUR
-    epoch = data["timestamp"]
-    time_stamp = datetime.fromtimestamp(epoch)
-    str_time_stamp = str(datetime.fromtimestamp(epoch))
-    header = ["base_EGP", "USD", "EUR", "time_stamp"]
-    row = [base, usd, eur, str_time_stamp]
-    return(header, row, time_stamp)
 
 
 def write_partitioned_data(partition_folder_path: str, partition_file_path: str, header: list, row: list) -> None:
@@ -65,6 +31,42 @@ def write_partitioned_data(partition_folder_path: str, partition_file_path: str,
                 csv_writer.writerow(row)
 
 
+def convert_rates(data: dict) -> tuple:
+    """function to work-around the currency conversion as the free API subscription doesn't allow to change the base"""
+    data = get_exchange_rates()
+    base = "1"
+    usd = str(1 / Decimal(data["rates"]["EGP"])) #EGP to USD
+    eur = str((1 / Decimal(data["rates"]["EGP"])) * Decimal(data["rates"]["EUR"])) #EGP to EUR
+    epoch = data["timestamp"]
+    time_stamp = datetime.fromtimestamp(epoch)
+    str_time_stamp = str(datetime.fromtimestamp(epoch))
+    header = ["base_EGP", "USD", "EUR", "time_stamp"]
+    row = [base, usd, eur, str_time_stamp]
+    return(header, row, time_stamp)
+
+
+def get_exchange_rates() -> dict:
+    """function to call the API and get the latest exchange rates for specific symbols(that are defined in the api_configs.py file)"""
+    full_url = api_configs.URL + api_configs.TOKEN + "&base=" + api_configs.BASE + "&symbols=" + api_configs.SYMBOLS
+    try:
+        response = requests.get(full_url)
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong with the api call, please check it")
+        print(e)
+        sys.exit(1)
+    json_payload = response.text
+    dict_payload = json.loads(json_payload)
+    return dict_payload
+
+
+def get_argument() -> argparse.Namespace:
+    """function takes args from the user with the targeted directory for the partitioned data and has a default value for the cwd"""
+    parser = argparse.ArgumentParser("exchange rate script")
+    parser.add_argument("-d", "--dir", default=".", help="directory of the file for the exchange rates, default is current directory")
+    args = parser.parse_args()
+    return args
+
+
 def main():
     args = get_argument()
     dir = args.dir
@@ -80,4 +82,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
